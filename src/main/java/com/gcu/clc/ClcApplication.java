8543732ba67fa6e2ba4ce8aee49c6767781c86b3
ntqams30 <msievers@thegeneral.com>
2021-11-16 09:48:28 -0600

    initial commit

---
+++ src/main/java/com/gcu/clc/ClcApplication.java
@@ -0,0 +1,13 @@
+package com.gcu.clc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gcu/clc/ClcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.clc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
