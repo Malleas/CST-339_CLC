51df2cf639fc04400f169a680e3014b18d9fbb10
ntqams30 <msievers@thegeneral.com>
2021-12-13 11:56:39 -0600

    Ending week docs

---
+++ documents/role.sql
@@ -0,0 +1,4 @@
+insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (1, 'admin');
+insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (2, 'manager');
+insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (3, 'employee');
+insert into ROLE(ROLE_ID, ROLE_NAME) VALUES (4, 'user');
