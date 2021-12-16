0c324e531c17d0f9d8c770d28a178c40473c5057
cylon-raider <markel74@msn.com>
2021-12-15 19:16:56 -0700

    Week 6 CLC initial commit

---
+++ src/main/java/com/gcu/clc/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.gcu.clc;
+
+import com.gcu.clc.business.UserBusinessService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    UserBusinessService service;
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/registration/**", "/images/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .permitAll()
+                .defaultSuccessUrl("/index", true)
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .permitAll()
+                .logoutSuccessUrl("/");
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.userDetailsService(service).passwordEncoder(passwordEncoder);
+    }
+
+
+
+
+
+}
