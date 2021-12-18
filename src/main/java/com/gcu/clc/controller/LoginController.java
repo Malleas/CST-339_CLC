d997564c89209780f51d8a225c21955800a89f16
Kdkocian <krencent870@Gmail.com>
2021-12-18 09:34:35 -0600

    Removed /DoLogin from LoginContoller, Removed SecurityBusinessService from Login Controller, REmoved SecurityBusinessService and SecurityDataSErvice classes.

--- src/main/java/com/gcu/clc/controller/LoginController.java
+++ src/main/java/com/gcu/clc/controller/LoginController.java
@@ -1,22 +1,13 @@
 package com.gcu.clc.controller;
 
-import com.gcu.clc.business.SecurityBusinessService;
 import com.gcu.clc.model.LoginModel;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.Valid;
 
 @Controller
 
 public class LoginController {
-
-    @Autowired
-    private SecurityBusinessService security;
 
     @GetMapping("/login")
     public ModelAndView display(){
@@ -27,23 +18,4 @@ public class LoginController {
         modelAndView.setViewName("login");
         return modelAndView;
     }
-    @PostMapping("doLogin")
-    public String doLogin(@Valid LoginModel loginModel, BindingResult bindingResult){
-        ModelAndView modelAndView = new ModelAndView();
-        if(bindingResult.hasErrors()){
-            modelAndView.addObject("title", "Login Form");
-            return "login";
-        }
-        /**
-         * Currently SecurityDataService returns the USER_ID if the username and password match.  We'll use this in the
-         * session at some point.
-         */
-        int loginStatus = security.authenticate(loginModel.getUsername(), loginModel.getPassword());
-        if(loginStatus == 0){
-            return "login";
-        }else {
-            return "index";
-        }
-    }
-
 }
